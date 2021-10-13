193e8cc94a68075f550c76abf658688063b5816a
Glyde <anton123454321045@yandex.by>
2021-10-13 11:40:40 +0300

    skeleton

---
+++ FakerLib/FakerLib.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace FakerLib
+{
+    public class Faker
+    {
+
+        private Dictionary<Type, ISimpleTypeGenerator> simpleTypeGenerator;
+        private Dictionary<Type, IGenericGenerator> genericTypeGenerator;
+        private List<Type> GeneratedTypesInClass;
+        
+        
+        public Faker()
+        {
+            this.simpleTypeGenerator = SimpleTypesCreator.getSimpleTypes();
+            this.genericTypeGenerator = new Dictionary<Type, IGenericGenerator>();
+
+            this.GeneratedTypesInClass = new List<Type>();
+        }
+
+        object Create(Type type)
+        {
+            object result = null;
+
+            return result;
+        }
+
+
+        public T Create<T>()
+        {
+            return (T)Create(typeof(T));
+        }
+
+        private object createClass(Type type)
+        {
+            object createdClass = null;
+            int maxLenConstructor = 0;
+            ConstructorInfo constructor = null;
+
+            var classConstructors = type.GetConstructors(BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public);
+
+            foreach (ConstructorInfo construct in classConstructors)
+            {
+                var num = construct.GetParameters().Length;
+
+                if (num > maxLenConstructor)
+                {
+                    maxLenConstructor = num;
+                    constructor = construct;
+                }
+            }
+
+            GeneratedTypesInClass.Add(type);
+
+
+            if (constructor != null)
+            {
+                createdClass = CreateFromConstructor(constructor, type);
+            }
+
+
+            GeneratedTypesInClass.Remove(type);
+
+            return createdClass;
+        }
+
+        private object CreateFromConstructor(ConstructorInfo constructor, Type type)
+        {
+            var parameters = new List<object>();
+
+            foreach (ParameterInfo parameterInfo in constructor.GetParameters())
+            {
+                object value = Create(parameterInfo.ParameterType);
+                parameters.Add(value);
+            }
+
+            return constructor.Invoke(parameters.ToArray());
+
+        }
+
+
+
+
+    }
+}
---
+++ FakerLib/SimpleTypesCreator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FakerLib
+{
+    class SimpleTypesCreator
+    {
+        public static void addDict(Dictionary<Type, ISimpleTypeGenerator> dict, ISimpleTypeGenerator generator)
+        {
+            dict.Add(generator.type, generator);
+        }
+
+        public static Dictionary<Type, ISimpleTypeGenerator> getSimpleTypes()
+        {
+            var dict = new Dictionary<Type, ISimpleTypeGenerator>();
+
+            //todo
+            //add to dictionary simple types
+
+            return dict;
+        }
+
+    }
+}
